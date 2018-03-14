affce3c7a48c7d949b4c7bc8f2cd3b3c7472614b
Galileorobotics <info@galileorobotics.ch>
2018-03-14 18:28:58 +0100

    Add water boundary & collision

--- waterLeft.gd
+++ waterLeft.gd
@@ -1,12 +1,11 @@
 extends Sprite
 
 const WATER_SPEED = 0.5
-
-func _ready():
-	pass
+const MIN_WATER_Y = 500
+const MAX_WATER_Y = 270
 
 func _process(delta):
-	if(Input.is_action_pressed("player2_up") && position.y > 500):
+	if(Input.is_action_pressed("player2_up") && position.y > MAX_WATER_Y):
 		position.y -= WATER_SPEED
-	if(Input.is_action_pressed("player2_down") && position.y < 1024):
+	if(Input.is_action_pressed("player2_down") && position.y < MIN_WATER_Y):
 		position.y += WATER_SPEED
