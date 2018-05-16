6beab47a849d1bd41b3cf7d08a50db813bf12063
GalileoRobotics <info@galileorobotics.ch>
2018-05-16 19:51:54 +0200

    Add fancy splash (WIP)

--- hippoLeft.gd
+++ hippoLeft.gd
@@ -1,6 +1,18 @@
 extends KinematicBody2D
 
+onready var splash = preload("res://splash.tscn")
+var s 
+
 const SPEED = 2
+const BOOST = 500
+
+func _ready():
+	s = splash.instance()
+	s.position.y -= 30
+	s.position.x += 110
+	s.get_child(0).connect("animation_finished", self, "deleteChild")
+	add_child(s)
+	s.hide()
 
 func _process(delta):
 	move_and_collide(Vector2(0, 1))
@@ -8,3 +20,14 @@ func _process(delta):
 		move_local_x(-SPEED)
 	if (Input.is_action_pressed("playerLeft_right")):
 		move_local_x(SPEED)
+	if (Input.is_action_just_pressed("playerLeft_up") && !s.get_child(0).is_playing()):
+		s.show()
+		s.get_child(0).play("splash")
+		var dist = sqrt(pow($"/root/main/ball".global_position.x-s.global_position.x,2) + (pow($"/root/main/ball".global_position.y-s.global_position.y,2)))
+		print(dist)
+		if(dist < BOOST):
+			$"/root/main/ball".apply_impulse(Vector2(0,0), Vector2(BOOST-dist,-BOOST+dist))
+
+
+func deleteChild(done):
+	s.hide()
--- hippoRight.gd
+++ hippoRight.gd
@@ -1,6 +1,18 @@
 extends KinematicBody2D
 
+onready var splash = preload("res://splash.tscn")
+var s 
+
 const SPEED = 2
+
+func _ready():
+	s = splash.instance()
+	s.flip_h = true
+	s.position.y -= 35
+	s.position.x -= 90
+	s.get_child(0).connect("animation_finished", self, "deleteChild")
+	add_child(s)
+	s.hide()
 
 func _process(delta):
 	move_and_collide(Vector2(0, 1))
@@ -8,3 +20,10 @@ func _process(delta):
 		move_local_x(-SPEED)
 	if (Input.is_action_pressed("playerRight_right")):
 		move_local_x(SPEED)
+	if (Input.is_action_just_pressed("playerRight_up")  && !s.get_child(0).is_playing()):
+		s.show()
+		s.get_child(0).play("splash")
+		$"/root/main/ball".apply_impulse(Vector2(s.position.x,s.position.y), Vector2(-100,-100))
+
+func deleteChild(done):
+	s.hide()
