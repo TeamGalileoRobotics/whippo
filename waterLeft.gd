d155a329024c0294ad05314cacfbd950caf07d4d
GalileoRobotics <info@galileorobotics.ch>
2018-05-16 18:10:45 +0200

    Remove water movement :(

--- waterLeft.gd
+++ waterLeft.gd
@@ -1,11 +1,4 @@
 extends StaticBody2D
 
-const WATER_SPEED = 0.5
-const MIN_WATER_Y = 500
-const MAX_WATER_Y = 270
-
 func _process(delta):
-	if(Input.is_action_pressed("playerRight_up") && position.y > MAX_WATER_Y):
-		position.y -= WATER_SPEED
-	if(Input.is_action_pressed("playerRight_down") && position.y < MIN_WATER_Y):
-		position.y += WATER_SPEED
+	pass
